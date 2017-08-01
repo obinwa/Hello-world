f8e995d4319dc539cb5ddb07d222dc698ec868ac
obinwa <30531212+obinwa@users.noreply.github.com>
2017-08-01 15:22:02 +0100

    Add files via upload
    testing

---
+++ NewSample.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div>
+	<p>My name is Chidiebere</p>
+	<p>Playing with superscrpt and subscript <sup>tty</sup><sub>gjd</sub></p>
+
+	<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;">
+</canvas>
+</div>
+</body>
+
+<script type="text/javascript">
+	var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(50,50);
+ctx.lineTo(150,50);
+ctx.stroke();
+</script>
+</html>
